fb3b953cb01e0155e38dd20206c5b332fa6e44e3
20110305@student.hcmute.edu.vn <20110305@student.hcmute.edu.vn>
2022-11-19 09:37:29 +0700

    tất cả sản phẩm

--- Do an TMDT/Controllers/HomeController.cs
+++ Do an TMDT/Controllers/HomeController.cs
@@ -24,6 +24,70 @@ namespace Do_an_TMDT.Controllers
             _context = context;
 
         }
+        public IActionResult Loadhet(int? page, int MaLoai)
+        {
+
+            var pageNumber = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 8;
+            var listcate = _context.ThuongHieus.AsNoTracking().ToList();
+            ViewBag.listcate = listcate;
+            HomeVM model = new HomeVM();
+            var listSP = _context.MatHangs.AsNoTracking()
+                .Where(x => x.DangDuocBan == true)
+                .ToList();
+            List<MatHangHome> listSPW = new List<MatHangHome>();
+            var listanh = _context.MatHangAnhs
+                .AsNoTracking()
+                .ToList();
+            var listTH = _context.ThuongHieus
+                .AsNoTracking()
+                .ToList();
+            foreach (var item_TH in listTH)
+            {
+                model.TH = listTH.Where(x => x.MaThuongHieu != null).ToList();
+            }
+            foreach (var item in listSP)
+            {
+
+                MatHangHome mh = new MatHangHome();
+                mh.listSPs = item;
+                foreach (var item_anh in listanh)
+                {
+                    mh.MatHangAnhs = listanh.Where(x => x.MaMatHang == item.MaMatHang).ToList();
+                }
+                foreach (var item_TH in listTH)
+                {
+                    mh.thuonghieu = listTH.Where(x => x.MaThuongHieu == item.MaThuongHieu).ToList();
+                }
+                listSPW.Add(mh);
+                model.MatHangs = listSPW;
+
+
+            }
+            if (MaLoai == null)
+            {
+                MaLoai = listcate[0].MaThuongHieu;
+            }
+            int j = 0;
+            foreach (var item2 in model.MatHangs.Where(x => x.listSPs.MaThuongHieu == MaLoai).ToList())
+            {
+                model.MatHangs[j] = item2; ;
+                j++;
+            }
+            ViewBag.TH = model.MatHangs;
+            int i = 0;
+            HttpContext.Session.SetInt32("Ten", i);
+
+
+
+
+
+            PagedList<MatHangHome> models = new PagedList<MatHangHome>(model.MatHangs.AsQueryable(), pageNumber, pageSize);
+            ViewBag.CurrentPage = pageNumber;
+            ViewBag.list = models;
+            return View(model);
+        }
+
         public IActionResult ThuongHieu(int MaTH)
         {
 
